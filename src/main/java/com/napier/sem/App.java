a0d60d13d410d3ac0b0659f687b75af7129f0a58
Gavala97 <40209147@live.napier.ac.uk>
2022-03-08 17:02:54 +0000

    Fixing DB Connection

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,6 +6,7 @@ public class App
 {
     public static void main(String[] args)
     {
+        //Initialise the DB
         try
         {
             // Load Database driver
@@ -28,7 +29,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:33060/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://world_db:33060/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 // Wait a bit
                 Thread.sleep(10000);
