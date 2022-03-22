069b1aff4f961243846ccbd13134f9617715044b
JamesMcfaul <40509181@live.napier.ac.uk>
2022-03-22 15:38:38 +0000

    City app.

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,22 @@
+package com.napier.sem;
+/** city object */
+public class City {
+    /** ID column */
+    public int iD;
+
+    /**  City name string column  */
+    public String name;
+
+    /** Country Code sting column  */
+    public String countryCode;
+
+    /** District string column */
+    public String district;
+
+    /** Population int column */
+
+    public int population;
+
+    /**  Country string column */
+    public String country;
+}
