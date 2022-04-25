e68b23baccdada8bbb71311dd64ba47273fa5db4
JamesMcFaul <40509181@live.napier.ac.uk>
2022-04-25 19:59:20 +0100

    country language class

---
+++ src/main/java/com/napier/sem/CountryLanguage.java
@@ -0,0 +1,22 @@
+package com.napier.sem;
+/**
+ * Represents a the langauge of the country
+ */
+public class CountryLanguage {
+    /**
+     * Code of the Country
+     */
+    public String countriesCode;
+    /**
+     * Language
+     */
+    public String language;
+    /**
+     * Official language of the country
+     */
+    public String isOfficial;
+    /**
+     * Percentage of speakers of a language in the country
+     */
+    public double percentage;
+}
